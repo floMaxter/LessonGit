4c79319f3a52d062ddb010707f2204f5d5517121
Max Vdovin <maxpostupaet@gmail.com>
2021-07-19 17:05:27 +0700

    Add the third line

--- Main.cpp
+++ Main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+using namespace std;
 
 int main()
 {
 
 	std::cout << "Hello, World!" << std::endl;
 	std::cout << "Other string!" << std::endl;
+	std::cout << "I love you!" << std::endl;
 
 
 	return 0;
